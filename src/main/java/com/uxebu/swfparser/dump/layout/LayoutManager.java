253d4216be7ec217c42bc8deab5cb15254ffe9e9
Ricardo Borillo <borillo@gmail.com>
2013-06-04 23:58:17 +0200

    Default implementation for addSprint code

--- src/main/java/com/uxebu/swfparser/dump/layout/LayoutManager.java
+++ src/main/java/com/uxebu/swfparser/dump/layout/LayoutManager.java
@@ -1,6 +1,7 @@
 package com.uxebu.swfparser.dump.layout;
 
 import java.io.File;
+import java.io.FileOutputStream;
 
 public class LayoutManager
 {
@@ -40,7 +41,18 @@ public class LayoutManager
 
     public void addSprite(String fileName, String content)
     {
-
+        try
+        {
+            FileOutputStream spriteFile = new FileOutputStream(outputDirectory + "/sprites/" + fileName);
+            spriteFile.write(content.getBytes());
+            spriteFile.flush();
+            spriteFile.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            throw new RuntimeException("Error creating spring file " + fileName);
+        }
     }
 
     public void addClass(String fileName, String content)
