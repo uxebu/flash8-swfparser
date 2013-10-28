4fa3618f357852fa58eb2223aa19f9d0bdaa2e04
Wolfram Kriesing <wk@uxebu.com>
2013-10-28 14:51:35 +0100

    Get the last statement and render all together as WHILE's condition. Still not generic enough, since only the last statement is used, not all that are needed.

--- src/main/java/org/swfparser/operation/WhileOperation.java
+++ src/main/java/org/swfparser/operation/WhileOperation.java
@@ -7,9 +7,11 @@
 
 package org.swfparser.operation;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
+import org.swfparser.BooleanOperation;
 import org.swfparser.ExecutionContext;
 import org.swfparser.Operation;
 import org.swfparser.exception.StatementBlockException;
@@ -17,8 +19,14 @@ import com.jswiff.swfrecords.actions.Action;
 
 public class WhileOperation extends IfOperation {
 
-	public WhileOperation(Stack<Operation> stack, List<Action> actions, ExecutionContext context) throws StatementBlockException {
+    private List<Operation> conditionStatments = new ArrayList<>();
+
+	public WhileOperation(Stack<Operation> stack, List<Operation> statements, List<Action> actions, ExecutionContext context) throws StatementBlockException {
 		super(stack, actions, context);
+        conditionStatments.add(0, this.condition);
+// TODO !!!!!! GET and remove ALLLL statements until where the while-block starts (the start label)
+        conditionStatments.add(0, statements.get(statements.size()-1));
+        statements.remove(statements.size()-1);
 		
 		// if "continue" is the last operation, remove it
 		if (!operations.isEmpty()) {
@@ -35,7 +43,39 @@ public class WhileOperation extends IfOperation {
 	protected String getLoopHeader() {
 		return "while";
 	}
-	
+
+    private String renderConditionStatements() {
+        StringBuilder buf = new StringBuilder();
+        Operation lastOp = conditionStatments.get(conditionStatments.size() - 1);
+        for (Operation op : conditionStatments) {
+            if (lastOp != op) {
+                buf.append(op.getStringValue(0));
+                buf.append(", ");
+            } else {
+                // The last op is the one that is actually the relevant one that is evaluated, e.g. in
+                //    while (__reg1--, __reg1 - 1 > -1)
+                // that needs to be inverted, since IF actions work like that.
+                buf.append(((BooleanOperation) op).getInvertedOperation().getStringValue(0));
+            }
+        }
+        return buf.toString();
+    }
+
+   protected String getHeaderLine() {
+        StringBuilder sourceCode = new StringBuilder();
+        sourceCode.append(getLoopHeader());
+        sourceCode.append(" (");
+//           sourceCode.append(((BooleanOperation) condition).getInvertedOperation().getStringValue(0));
+        sourceCode.append(renderConditionStatements());
+        sourceCode.append(") {");
+        return sourceCode.toString();
+   	}
+
+    @Override
+   	public String toString() {
+   		return "while(" + renderConditionStatements() + ")";
+   	}
+
 	public Operation getCondition() {
 		return condition;
 	}
